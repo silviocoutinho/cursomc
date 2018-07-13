fd2b950c4f65c82cc92d0c6567e344ecaf435cad
Silvio Coutinho <silviofc79@gmail.com>
2018-07-12 21:32:32 -0300

    Criando e Testando a classe Categoria

--- src/main/java/com/silviocoutinho/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/silviocoutinho/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.silviocoutinho.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.silviocoutinho.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias") //criacao do endpoint categorias
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)//usando o verbo GET
-	public String listar() {
-		return "REST OK!!!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
